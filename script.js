ecc118b40cd2af1a4449caf8eca742b59f214e80
Glitch (hello-webpage) <none>
2021-03-18 21:31:01 +0000

    📡🏡 Checkpoint ./script.js:29897639/563 ./index.html:29897639/263 ./style.css:29897639/274

--- script.js
+++ script.js
@@ -124,5 +124,33 @@ function winGame(){
   alert("Game Over.You won!")
 
 }
+function guess(btn){
+  console.log("user guessed: " + btn);
+
+  if(!gamePlaying){
+    return;
+  }
+
+  if(pattern[guessCounter] == btn){
+    //Guess was correct!
+    if(guessCounter == progress){
+      if(progress == pattern.length - 1){
+        //GAME OVER: WIN!
+        winGame();
+      }else{
+        //Pattern correct. Add next segment
+        progress++;
+        playClueSequence();
+      }
+    }else{
+      //so far so good... check the next guess
+      guessCounter++;
+    }
+  }else{
+    //Guess was incorrect
+    //GAME OVER: LOSE!
+    loseGame();
+  }
+}    
 
 
