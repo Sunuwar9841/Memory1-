f5ae5632c8e132987bee0da4813f1b1a9cbda43d
Glitch (hello-webpage) <none>
2021-03-18 18:51:20 +0000

    🚥🚔 Checkpoint ./script.js:29897639/1439

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+//Global Variable
+var pattern = [1,1,4,3,1,2,1,3];  //keeps track of the secret pattern the button presses
+//var pattern = [];  //keeps track of the pattern the button presses
+var progress = 0  //keep track of progress and status
+var gamePlaying = false // keeps track od game is currently active or not
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+function startGame(){
+  progress = 0;
+  gamePlaying = true;
+  document.getElementById("startBtn").classList.add("hidden");
+  document.getElementById("stopBtn").classList.add("hidden");
+  
+  }
+
+function stopGame(){
+  gamePlaying = false;
+  document.getElementById("startBtn").classList.add("hidden");
+  document.getElementById("stopBtn").classList.add("hidden");
+  
+  }
